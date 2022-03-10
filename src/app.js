efdc2f85408b2dc9153688ef341476dbc0eaf317
thaotp-98 <81809411+thaotp-98@users.noreply.github.com>
2022-03-10 15:35:48 +0700

    day2

--- src/app.js
+++ src/app.js
@@ -1,35 +1,20 @@
-const express = require('express');
-const cors  = require('cors');
+// const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import productRoute from './routers/products';
+import morgan from 'morgan';
 
 const app = express();
-app.use(cors());
+// middleware
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
 
+// routes
+app.use("/api", productRoute)
 
-const checkAuth = (req, res, next) => {
-   const isAdmin = false;
-   if(isAdmin) {
-      next();
-   }else{
-      console.log("you not a admin");
-   }
-}
-app.get('/', (req, res) => { res.send("<h1>Home Page</h1>") });
-
-app.get('/api/users',checkAuth, (req, res,next) => {
-  const data = [
-     {id: 1 , name: 'John', status: 'active',address: '123 Main Street'},
-     {id: 2 , name: 'Lalisa', status: 'active',address: '123 Main Street'},
-     {id: 3 , name: 'Rose', status: 'active',address: '123 Main Street'},
-     {id: 4 , name: 'Jennie', status: 'active',address: '123 Main Street'},
-     {id: 5 , name: 'Karina', status: 'active',address: '123 Main Street'},
-  ];
-  
-   res.json(data) 
-})
-
-
-
+// connect
 const PORT = 3001;
 app.listen(PORT, () => {
-   console.log(`Server run in ${PORT} port`);
+    console.log("Server của bạn đang chạy cổng ", PORT);
 });
